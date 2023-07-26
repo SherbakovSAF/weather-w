258c5a3b51aa26cdcd5a95a60e742f1e932d45eb
SherbakovSAF <sherbakovsaf@gmail.com>
2023-07-26 17:34:19 +0300

    Инициализация проекта

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <weather-widgets/>
+</template>
+
+<script>
+import WeatherWidgets from './components/WeatherWidgets.vue'
+
+export default {
+  name: 'App',
+  components: {
+    WeatherWidgets
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
---
+++ src/components/WeatherWidgets.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WeatherWidgets',
+}
+</script>
+<style scoped lang="scss">
+
+</style>
