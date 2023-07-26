83bfcdf64be86c29a140ee5a7633fa7eecdd99f0
SherbakovSAF <sherbakovsaf@gmail.com>
2023-07-26 23:03:35 +0300

    Добавление первоначальной вёрстки

---
+++ src/components/WeatherWidgetsComponents/MainWindow.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <div>
+      <h1>London, UK</h1>
+      <button>settings</button>
+    </div>
+    <div>Feels like <span>-3C</span>. Broken clouds. Light breeze</div> 
+    <div>
+      <div>
+        <img src="" alt="Direction">
+        <div><span>3m/s</span>SSE</div>
+      </div>
+      <div>
+        <img src="" alt="Compas">
+        <div>1021hPA</div>
+      </div>
+    </div>
+    <div>
+      <div>
+        Humidity: <span>97%</span>
+      </div>
+      <div>
+        Dew point: <span>0C</span>
+      </div>
+    </div>
+    <div>Visibility 10km</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MainWindowWeatherWidgets',
+}
+</script>
+<style scoped lang="scss">
+
+</style>
---
+++ src/components/WeatherWidgetsComponents/SettingsWindow.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div>
+      <h1>London, UK</h1>
+      <button>Close</button>
+    </div>
+    <div>
+      <article>
+        <div>Иконка</div>
+        <h1>London</h1>
+        <button>Delete</button>
+      </article>
+    </div>
+    <div>
+      <h2>Add Location</h2>
+      <div>
+        <input type="text">
+        <button>Enter</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MainWindowWeatherWidgets',
+}
+</script>
+<style scoped lang="scss">
+
+</style>
